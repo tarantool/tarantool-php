42a63b4aeb43ef75f0b13bd3a7341c7897ffa43c
Eugine Blikh <bigbes@gmail.com>
2018-08-13 14:52:12 +0000

    Do not reload schema on failed authentication
    closes gh-71

--- src/tarantool.c
+++ src/tarantool.c
@@ -1184,7 +1184,6 @@ PHP_METHOD(Tarantool, reconnect) {
 static int __tarantool_authenticate(tarantool_connection *obj) {
 	TSRMLS_FETCH();
 
-	tarantool_schema_flush(obj->schema);
 	tarantool_tp_update(obj->tps);
 	int batch_count = 3;
 	size_t passwd_len = (obj->passwd ? strlen(obj->passwd) : 0);
@@ -1232,7 +1231,9 @@ static int __tarantool_authenticate(tarantool_connection *obj) {
 			status = FAILURE;
 		}
 		if (status != FAILURE) {
-			if (resp.sync == space_sync && tarantool_schema_add_spaces(
+			if (resp.sync == auth_sync) {
+				tarantool_schema_flush(obj->schema);
+			} else if (resp.sync == space_sync && tarantool_schema_add_spaces(
 					obj->schema,
 					resp.data,
 					resp.data_len) == -1) {
