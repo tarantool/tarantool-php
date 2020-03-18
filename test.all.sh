176d49f489f78fcea0fd5b984bb7474d0dce68d2
Alexander Turenko <alexander.turenko@tarantool.org>
2020-03-18 13:11:16 +0300

    test: allow to verify all configurations locally
    Provide the script, which run the whole test suite for the connector on
    all supported tarantool and php versions. It is useful to verify changes
    locally.
    
    The script uses php:<version>-cli docker images and the ./test.sh script
    inside (exactly what is called on Travis-CI). The ./test.sh script is
    modified to work under those images:
    
    * install necessary packages (gnupg2, lsb-release, apt-transport-https),
      when when they're not installed
    * support tarantool repositories for Debian (not only Ubuntu)
    * don't require 'sudo' when the current user is root (usual for docker)
    * don't require 'pip', install python-yaml from packages (pip is not
      available by default in php's images)
    * while we're here, changed GPG key and repository URLs from http:// to
      https:// (it is redirected to https:// anyway)
    * fix tarantool-1.6 installation on Debian Stretch
    
    Downside: APT repositories updating takes significant time and it is
    performed for each configuration.

---
+++ test.all.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+set -exu  # Strict shell (w/o -o pipefail)
+
+php_version_list="7.0"
+tarantool_version_list="1.6 1.7 1.9 1.10 2.1 2.2 2.3 2.4"
+
+# gh-151: disable tarantool-2.2 due to lack of support the new
+# _index format.
+exceptions="
+    php-7.0-tarantool-2.2
+    php-7.1-tarantool-2.2
+    php-7.2-tarantool-2.2
+    php-7.3-tarantool-2.2
+    php-7.4-tarantool-2.2
+"
+
+for php_version in $php_version_list; do
+    for tarantool_version in $tarantool_version_list; do
+        echo "======================================"
+        echo "PHP_VERSION=${php_version} TARANTOOL_VERSION=${tarantool_version}"
+        echo "======================================"
+
+        # Skip configurations from exceptions list.
+        conf="php-${php_version}-tarantool-${tarantool_version}"
+        if [ -z "${exceptions##*${conf}*}" ]; then
+            echo "*** Skip the configuration ***"
+            continue
+        fi
+
+        docker run                                       \
+            --volume $(realpath .):/tarantool-php        \
+            --workdir /tarantool-php                     \
+            --env TARANTOOL_VERSION=${tarantool_version} \
+            --rm                                         \
+            php:${php_version}-cli                       \
+            sh -c 'make clean; ./test.sh'
+    done
+done
--- test.sh
+++ test.sh
@@ -4,23 +4,59 @@ set -exu  # Strict shell (w/o -o pipefail)
 
 TARANTOOL_VERSION=${TARANTOOL_VERSION:-1.10}
 
-curl http://download.tarantool.org/tarantool/${TARANTOOL_VERSION}/gpgkey | \
-    sudo apt-key add -
-release=`lsb_release -c -s`
+if [ "$(id -u)" = "0" ]; then
+    SUDO=""
+else
+    SUDO="sudo"
+fi
 
-# append two lines to a list of source repositories
-sudo rm -f /etc/apt/sources.list.d/*tarantool*.list
-sudo tee /etc/apt/sources.list.d/tarantool_${TARANTOOL_VERSION}.list <<- EOF
-deb http://download.tarantool.org/tarantool/${TARANTOOL_VERSION}/ubuntu/ $release main
+${SUDO} apt-get update > /dev/null
+
+if [ -z "$(which gpg)" ]; then
+    ${SUDO} apt-get -qy install gnupg2 > /dev/null
+fi
+
+if [ -z "$(which lsb_release)" ]; then
+    ${SUDO} apt-get -qy install lsb-release > /dev/null
+fi
+
+${SUDO} apt-get -qy install apt-transport-https > /dev/null
+${SUDO} apt-get -qy install python-yaml > /dev/null
+
+OS="$(lsb_release --id --short | tr '[:upper:]' '[:lower:]')"  # debian or ubuntu
+DIST="$(lsb_release --codename --short)"                   # stretch, xenial, ...
+
+# Setup tarantool repository.
+curl https://download.tarantool.org/tarantool/${TARANTOOL_VERSION}/gpgkey | \
+    ${SUDO} apt-key add -
+${SUDO} rm -f /etc/apt/sources.list.d/*tarantool*.list
+${SUDO} tee /etc/apt/sources.list.d/tarantool_${TARANTOOL_VERSION}.list <<- EOF
+deb https://download.tarantool.org/tarantool/${TARANTOOL_VERSION}/${OS}/ ${DIST} main
 EOF
+${SUDO} apt-get update > /dev/null
 
-# install
-sudo apt-get update > /dev/null
-sudo apt-get -qy install tarantool tarantool-dev
-
+# Install tarantool executable and headers.
+#
+# Explicit version is necessary to install a lower tarantool
+# version from our repository when a system have a higher version
+# in default repositories. Say, Debian Stretch have tarantool-1.7
+# that prevents tarantool-1.6 from being installed from our repo
+# if we would not hold the version.
+${SUDO} apt-get -qy install "tarantool=${TARANTOOL_VERSION}*" \
+    "tarantool-dev=${TARANTOOL_VERSION}*" > /dev/null
 tarantool --version
+
+# Ensure we got the requested tarantool version.
+TARANTOOL_VERSION_PATTERN='^Tarantool \([0-9]\+\.[0-9]\+\)\..*$'
+ACTUAL_TARANTOOL_VERSION="$(tarantool --version | head -n 1 | \
+    sed "s/${TARANTOOL_VERSION_PATTERN}/\\1/")"
+if [ "${ACTUAL_TARANTOOL_VERSION}" != "${TARANTOOL_VERSION}" ]; then
+    echo "Requested tarantool-${TARANTOOL_VERSION}, got" \
+        "tarantool-${ACTUAL_TARANTOOL_VERSION}"
+    exit 1
+fi
+
 phpize && ./configure
 make
 make install
-sudo pip install PyYAML
 /usr/bin/python test-run.py
