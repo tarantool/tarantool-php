f0b3f1585124f65e3d18e9cdadf1063ac45c8a5b
Alexander Turenko <alexander.turenko@tarantool.org>
2020-03-26 02:20:21 +0300

    build/debian: eliminate hardcoded PHP 7.0 version
    Preprocess debian/control file to set proper package name like
    php7.3-tarantool. Use the package name in debian/rules file to determine
    installation paths.
    
    Removed php7.0-dev dependency, because php-all-dev already depends on
    proper php7.?-dev package.
    
    This change allows to build a Deb package for distributions, which
    provides PHP version other then 7.0. Those builds will be enabled in CI
    in further commits.
    
    Details
    -------
    
    It seems it is not possible to build a package with a conditional name
    (depending of a distribution). `man 5 deb-substvars` explicitly states
    that 'Package' field in debian/control can be parametrized using a
    substitution varible.
    
    Another possible way would be define several 'Package' fields in
    debian/control and build only one for each distribution. However I don't
    find a way to build only one package using `debuild` (it is used inside
    `packpack`).
    
    So it seems that the preprocessing is the only way to build, say,
    php7.0-tarantool package for Debian Stretch and php7.3-tarantool for
    Debian Buster.
    
    The preprocessing is performed from debian/prebuild.sh script, which is
    invoked by `packpack`. So nothing changed in the build process from a
    caller perspective.
    
    Part of #117

---
+++ debian/prebuild.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -exu  # Strict shell (w/o -o pipefail)
+
+# Preserve DEBIAN_FRONTEND=noninteractive. It prevents packages
+# like tzdata from asking configuration parameters interactively.
+#
+# See https://github.com/packpack/packpack/issues/7
+SUDO="sudo -E"
+
+${SUDO} apt-get update > /dev/null
+${SUDO} apt-get -y install php-all-dev
+
+phpversion=$(php-config --version | sed 's/^\([0-9]\+\.[0-9]\).*/\1/')
+
+cd /build/php-tarantool-*
+sed -e "s/\${phpversion}/${phpversion}/" debian/control.in > debian/control
+rm debian/control.in
