954203495e4a76250cead6ded64e5e30e68c4eda
Alexander Turenko <alexander.turenko@tarantool.org>
2020-03-25 21:11:19 +0300

    build: allow to test RPM / Deb packages locally
    Provided the script to build a RPM / Deb package, install it inside a
    docker container based on an appropriate image and verify that the main
    class of the extension (Tarantool) is available from the PHP
    interpreter.
    
    The script builds a package just like it is performed in CI: using
    packpack. It should be available in PATH directories.
    
    Only RPM packages are here now, but the following commits will enable
    Deb packages too.
    
    Part of #117

---
+++ test.pkg.all.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+set -exu  # Strict shell (w/o -o pipefail)
+
+distros="
+    el:8
+    fedora:25
+    fedora:26
+    fedora:27
+    fedora:28
+    fedora:29
+    fedora:30
+    fedora:31
+"
+
+if ! type packpack; then
+    echo "Unable to find packpack"
+    exit 1
+fi
+
+for distro in $distros; do
+    export OS="${distro%%:*}"
+    export DIST="${distro#*:}"
+    if [ "${OS}" = "el" ]; then
+        export OS=centos
+    fi
+
+    rm -rf build
+    packpack
+
+    docker run \
+        --volume "$(realpath .):/tarantool-php" \
+        --workdir /tarantool-php                \
+        --rm                                    \
+        "${OS}:${DIST}"                         \
+        ./test.pkg.sh
+done
